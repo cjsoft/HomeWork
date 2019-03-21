4c7611cbd885db071769cc9d40374a1aaf5a2303
CJSoft <egwcyh@qq.com>
2019-03-21 20:16:30 +0800

    OOP foundation exp complete

---
+++ oop/exp5/1/usingvirtual/shape.h
@@ -0,0 +1,46 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+class Shape {
+public:
+    Shape();
+    ~Shape();
+    virtual int getArea() const = 0;
+};
+
+class Rectangle : public Shape {
+private:
+    int width, height;
+public:
+    Rectangle();
+    ~Rectangle();
+    void setWidth(int w);
+    void setHeight(int h);
+    int getWidth() const;
+    int getHeight() const;
+    virtual int getArea() const;
+};
+
+class Square : public Rectangle {
+private:
+    void setWidth(int w);
+    void setHeight(int h);
+public:
+    Square();
+    ~Square();
+    void setSideLength(int d);
+    int getSideLength() const;
+    virtual int getArea() const;
+};
+
+class Circle : public Shape {
+private:
+    int radius;
+public:
+    Circle();
+    ~Circle();
+    void setRadius(int r);
+    int getRadius() const;
+};
+
+#include "shape.cpp"
+#endif
