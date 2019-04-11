bc64892be4bd8cc685d5035c07b26071b3cdb7b0
CJSoft <egwcyh@qq.com>
2019-04-11 17:27:37 +0800

    algo dp flower shop util almost finish

--- algo/dynamic programming/little shop of flowers/test.sh
+++ algo/dynamic programming/little shop of flowers/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in case1.txt case2.txt case3.txt case4.txt case5.txt case6.txt case7.txt
+for i in ./data/case1.txt ./data/case2.txt ./data/case3.txt ./data/case4.txt ./data/case5.txt ./data/case6.txt ./data/case7.txt
 do
     if [ "$(diff -Bb <(./dp < $i | head -n 1) <(./dnc < $i))" != "" ]
     then
